1a778c3b9cf0d58e1e66d1f8450519fdfa5411ce
mbembe <mbembesheb@gmail.mail>
2022-03-24 11:23:40 +0100

    diagramme et script

---
+++ leonbdapp/monde.sql
@@ -0,0 +1,45 @@
+/*==============================================================*/
+/* Nom de SGBD :  MySQL 5.0                                     */
+/* Date de création :  21/03/2022 16:19:13                      */
+/*==============================================================*/
+
+
+drop table if exists Apprenants;
+
+drop table if exists Tuteur;
+
+/*==============================================================*/
+/* Table : Apprenants                                           */
+/*==============================================================*/
+create table Apprenants
+(
+   idApprenants         int not null,
+   idTuteur             int not null,
+   nom                  varchar(254),
+   prenom               varchar(254),
+   telephone            int,
+   adresse              varchar(254),
+   genre                varchar(254),
+   email                varchar(254),
+   primary key (idApprenants)
+);
+
+/*==============================================================*/
+/* Table : Tuteur                                               */
+/*==============================================================*/
+create table Tuteur
+(
+   idTuteur             int not null,
+   nom                  varchar(254),
+   prenom               varchar(254),
+   genre                varchar(254),
+   profession           varchar(254),
+   telephone            int,
+   adresse              varchar(254),
+   email                varchar(254),
+   primary key (idTuteur)
+);
+
+alter table Apprenants add constraint FK_association1 foreign key (idTuteur)
+      references Tuteur (idTuteur) on delete restrict on update restrict;
+
