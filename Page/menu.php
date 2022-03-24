d68a482981bbe91d3c5aa8744fffcd456d954948
Bethel Ngakoutou <ngaradoumbethel@gmail.com>
2022-03-24 10:50:49 +0000

    Ma partie_bethel

---
+++ Page/menu.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-light bg-danger mt-3">
+  <div class="container-fluid">
+      <a href="" class="navbar-brand"><img src="../Style/Images/logo.png" width="100px"></a>
+      <a href="#"><button class="btn btn-outline-light" style="width:150px" type="submit">Accueil</button></a>
+      <a href="../page/apprenant.php"><button class="btn btn-outline-light" style="width:150px" type="submit">Apprenant</button></a>
+      <a href="#"><button class="btn btn-outline-light" style="width:150px" type="submit">Tuteur</button></a>
+    <form class="d-flex">
+      <input class="form-control me-2" type="search" placeholder="Recherche" aria-label="Recherche">
+      <button class="btn btn-outline-light" type="submit">Recherche</button>
+    </form>
+  </div>
+</nav>
