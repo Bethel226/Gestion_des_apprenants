64fb8b3f56c3dc1e3c1229c45364dfa75986265a
mbembe <mbembesheb@gmail.mail>
2022-03-30 13:10:48 +0200

    index - leon

--- Page/foot.php
+++
@@ -1,3 +0,0 @@
-<nav class="container-fluid bg-danger mt-3">
-      <h3 class="text-light text-center">Copyright Begale 2022</h3>
-</nav>
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>App</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="../Style/css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@700&family=Lobster&family=Merriweather&family=Poppins&display=swap" rel="stylesheet"> 
+</head>
+<body>
+<?php include 'Page/menu_index.php'; ?>
+  <div class="container-fluid bg-danger mt-3 d-flex w-100"> 
+      <div class="bg-light w-30">
+          <h1 class="text-center mb-5" style="font-family: 'Lobster', 'cursive'; font-size: 4rem;">Bienvenue dans la gestion des apprenants de Simplon.Co</h1>
+          <p class="text-center text-danger" style="font-family: 'Lobster', 'cursive'; font-size: 2rem;">La solution pour un meilleur suivi des apprenants!</p> 
+      </div>
+      <div class="w-70">
+          <img src="Style/Images/accueil.Jpeg" style="height:460px " alt="">
+      </div>
+  </div>
+    <?php include 'Page/foot.php'; ?>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> 
+</body>
+</html>
